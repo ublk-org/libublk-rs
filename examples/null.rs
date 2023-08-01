0057d83ec0d4da92d07f36e2e540d2b9234ba674
Ming Lei <tom.leiming@gmail.com>
2023-08-01 03:20:19 +0000

    example: switch to low level APIs
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- examples/null.rs
+++ examples/null.rs
@@ -1,41 +1,47 @@
-use libublk::io::{UblkDev, UblkIOCtx, UblkQueueCtx};
+use libublk::io::{UblkDev, UblkIOCtx, UblkQueue, UblkQueueCtx};
 use libublk::{ctrl::UblkCtrl, UblkError};
+use std::sync::Arc;
 
-fn handle_io(ctx: &UblkQueueCtx, io: &mut UblkIOCtx) -> Result<i32, UblkError> {
+fn null_handle_io(ctx: &UblkQueueCtx, io: &mut UblkIOCtx) -> Result<i32, UblkError> {
     let iod = ctx.get_iod(io.get_tag());
     let bytes = unsafe { (*iod).nr_sectors << 9 } as i32;
 
     io.complete_io(bytes);
     Ok(0)
 }
+fn test_add(dev_id: i32) {
+    let nr_queues = 2; //two queues
+                       //io depth: 64, max buf size: 512KB
+    let mut ctrl = UblkCtrl::new(dev_id, nr_queues, 64, 512 << 10, 0, true).unwrap();
 
-fn test_add() {
-    let s = std::env::args().nth(2).unwrap_or_else(|| "-1".to_string());
-    let dev_id = s.parse::<i32>().unwrap();
-    let _pid = unsafe { libc::fork() };
-    if _pid == 0 {
-        libublk::ublk_tgt_worker(
-            "null".to_string(),
-            dev_id,
-            2,
-            64,
-            512_u32 * 1024,
-            0,
-            true,
-            |dev: &mut UblkDev| {
-                dev.set_default_params(250_u64 << 30);
-                Ok(serde_json::json!({}))
-            },
-            handle_io,
-            |dev_id| {
-                let mut ctrl = UblkCtrl::new(dev_id, 0, 0, 0, 0, false).unwrap();
-                ctrl.dump();
-            },
-        )
-        .unwrap()
-        .join()
-        .unwrap();
+    //target specific initialization is done in this closure
+    let tgt_init = |dev: &mut UblkDev| {
+        dev.set_default_params(250_u64 << 30);
+        Ok(serde_json::json!({}))
+    };
+    let ublk_dev =
+        std::sync::Arc::new(UblkDev::new("null".to_string(), tgt_init, &mut ctrl, 0).unwrap());
+    let mut threads = Vec::new();
+
+    for q in 0..nr_queues {
+        let dev = Arc::clone(&ublk_dev);
+        threads.push(std::thread::spawn(move || {
+            let mut queue = UblkQueue::new(q as u16, &dev).unwrap();
+            let ctx = queue.make_queue_ctx();
+
+            //IO handling closure(FnMut), we are driven by io_uring CQE, and
+            //this closure is called for every incoming CQE(io command or
+            //target io completion)
+            let io_handler = move |io: &mut UblkIOCtx| null_handle_io(&ctx, io);
+            queue.wait_and_handle_io(io_handler);
+        }));
     }
+    ctrl.start_dev(&ublk_dev).unwrap();
+    ctrl.dump();
+    for qh in threads {
+        qh.join().unwrap();
+    }
+    ctrl.stop_dev(&ublk_dev).unwrap();
 }
 
 fn test_del() {
@@ -49,7 +55,14 @@ fn test_del() {
 fn main() {
     if let Some(cmd) = std::env::args().nth(1) {
         match cmd.as_str() {
-            "add" => test_add(),
+            "add" => {
+                let s = std::env::args().nth(2).unwrap_or_else(|| "-1".to_string());
+                let dev_id = s.parse::<i32>().unwrap();
+                let _pid = unsafe { libc::fork() };
+                if _pid == 0 {
+                    test_add(dev_id);
+                }
+            }
             "del" => test_del(),
             _ => todo!(),
         }
