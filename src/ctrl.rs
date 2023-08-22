ublk-org/libublk-rs

commit b48cfc9ea2b4595c671ec67cb5227996f229403f

libublk: ctrl: flush json before end user recovery
Json export has been changed during recovery.

Signed-off-by: Ming Lei <tom.leiming@gmail.com>

src/ctrl.rs
@@ -646,6 +646,7 @@ impl UblkCtrl {
             self.flush_json()?;
             self.start(unsafe { libc::getpid() as i32 }, async_cmd)?
         } else {
+            self.flush_json()?;
             self.end_user_recover(unsafe { libc::getpid() as i32 }, async_cmd)?
         };
 

commit e31304e89e0025bf57274a44573aefb5a1bbb25b

libublk: ctrl: fix auto-population of dev_info and json reload
Only do that in case that one valid device ID is provided.

Fixes: b48cfc9 ("libublk: ctrl: flush json before end user recovery")
Fixes: f3c7a15 ("libublk: ctrl: populate dev_info in UblkDev::new() in case of !for_add")
Signed-off-by: Ming Lei <tom.leiming@gmail.com>

src/ctrl.rs
@@ -251,7 +251,7 @@ impl UblkCtrl {
         //add cdev if the device is for adding device
         if dev.for_add {
             dev.add()?;
-        } else {
+        } else if id >= 0 {
             let res = dev.reload_json();
             if res.is_err() {
                 eprintln!("device reload json failed");