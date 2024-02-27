acea1babe775761dd0a42c1e26ff8664c9337de3
Ming Lei <tom.leiming@gmail.com>
2024-02-27 01:14:45 +0000

    example/ramdisk: don't call ctrl.stop_dev() before exiting
    Our control device will stop & delete device automatically in its
    Drop trait method.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>

--- examples/ramdisk.rs
+++ examples/ramdisk.rs
@@ -118,10 +118,6 @@ fn rd_add_dev(dev_id: i32, buf_addr: *mut u8, size: u64, for_add: bool) {
     } else {
         eprintln!("device can't be started");
     }
-
-    //device may be deleted from another context, so it is normal
-    //to see -ENOENT failure here
-    let _ = ctrl.stop_dev();
 }
 
 fn rd_get_device_size(ctrl: &UblkCtrl) -> u64 {
